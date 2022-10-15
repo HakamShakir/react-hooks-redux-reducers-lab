f393170629840c89f1d34a26a207f75547f780e7
HakamShakir <hakamshakir2002@gmail.com>
2022-10-15 13:52:49 +0300

    Completed Assignment

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,14 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case actions.increase:
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
+
+const actions = {
+  increase: "presents/increase",
+};
